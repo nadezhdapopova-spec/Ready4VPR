202aaa811259dd7417d5b4430d7cc0ae713bd814
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-11-18 02:12:08 +0700

    created custom command create_test_data in users app

---
+++ users/management/commands/create_test_data.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+from lms.models import Course, Lesson
+from users.models import City, CustomUser, Payment
+
+
+class Command(BaseCommand):
+    help = "Add test users, courses, lessons, payments to the database from fixtures"
+
+    def handle(self, *args, **kwargs):
+        City.objects.all().delete()
+        CustomUser.objects.all().delete()
+        Lesson.objects.all().delete()
+        Course.objects.all().delete()
+        Payment.objects.all().delete()
+
+        call_command("loaddata", "users/fixtures/cities_fixture.json")
+        self.stdout.write(self.style.SUCCESS("Загружены города из фикструры"))
+
+        call_command("loaddata", "users/fixtures/users_fixture.json")
+        self.stdout.write(self.style.SUCCESS("Загружены пользователи из фикструры"))
+
+        call_command("loaddata", "lms/fixtures/courses_fixture.json")
+        self.stdout.write(self.style.SUCCESS("Загружены курсы из фикструры"))
+
+        call_command("loaddata", "lms/fixtures/lessons_fixture.json")
+        self.stdout.write(self.style.SUCCESS("Загружены уроки из фикструры"))
+
+        call_command("loaddata", "users/fixtures/payments_fixture.json")
+        self.stdout.write(self.style.SUCCESS("Загружены платежи из фикструры"))
+
+        self.stdout.write(self.style.SUCCESS("Данные успешно загружены"))
