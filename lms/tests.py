bb2cb683d1836b850d49706c6d0d150b3fe7e003
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-12-24 22:45:34 +0700

    modified tests

--- lms/tests.py
+++ lms/tests.py
@@ -39,116 +39,116 @@ class TestBaseLMSViewSet(APITestCase):
         self.client_stranger.force_authenticate(user=self.stranger)
 
 
-# class TestCourseViewSet(TestBaseLMSViewSet):
-#     """Тестирует корректность работы CRUD курсов"""
-#
-#     def setUp(self):
-#         """Формирует тестовые данные"""
-#         super().setUp()
-#         self.course = Course.objects.create(title="Test Course", owner=self.user)
-#
-#     def test_list_courses_authenticated(self):
-#         """Проверяет, что отображается список курсов для авторизованных пользователей"""
-#         url = reverse("lms:courses-list")
-#         response = self.client_user.get(url)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#
-#     def test_list_courses_unauthenticated(self):
-#         """Проверяет, что не отображается список курсов для неавторизованных пользователей"""
-#         url = reverse("lms:courses-list")
-#         client = APIClient()
-#         response = client.get(url)
-#         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-#
-#     def test_retrieve_course_unauthenticated(self):
-#         """Проверяет, что не отображается курс для неавторизованных пользователей"""
-#         url = reverse("lms:courses-detail", args=[self.course.id])
-#         client = APIClient()
-#         response = client.get(url)
-#         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-#
-#     def test_retrieve_course_non_owner_and_non_moderator(self):
-#         """Проверяет, что авторизованный не автор и не модератор не может просматривать курс"""
-#         url = reverse("lms:courses-detail", args=[self.course.id])
-#         response = self.client_stranger.get(url)
-#         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-#
-#     def test_retrieve_course_owner(self):
-#         """Проверяет, что отображается курс для автора"""
-#         url = reverse("lms:courses-detail", args=[self.course.id])
-#         response = self.client_user.get(url)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#
-#     def test_retrieve_course_moderator(self):
-#         """Проверяет, что отображается курс для модератора"""
-#         url = reverse("lms:courses-detail", args=[self.course.id])
-#         response = self.client_mod.get(url)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#
-#     def test_create_course_superuser(self):
-#         """Проверяет, что админ может создавать курс"""
-#         url = reverse("lms:courses-list")
-#         data = {"title": "New Course"}
-#         response = self.client_super.post(url, data)
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#         self.assertTrue(Course.objects.filter(title="New Course").exists())
-#
-#     def test_create_course_normal_user(self):
-#         """Проверяет, что авторизованный пользователь может создавать курс"""
-#         url = reverse("lms:courses-list")
-#         data = {"title": "User Course"}
-#         response = self.client_user.post(url, data)
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#         self.assertTrue(Course.objects.filter(title="User Course").exists())
-#
-#     def test_create_course_moderator(self):
-#         """Проверяет, что модератор не может создавать курс"""
-#         url = reverse("lms:courses-list")
-#         data = {"title": "New Course"}
-#         response = self.client_mod.post(url, data)
-#         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-#
-#     def test_update_course_owner(self):
-#         """Проверяет, что автор курса может редактировать курс"""
-#         url = reverse("lms:courses-detail", args=[self.course.id])
-#         data = {"title": "Updated Course"}
-#         response = self.client_user.patch(url, data)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.course.refresh_from_db()
-#         self.assertEqual(self.course.title, "Updated Course")
-#
-#     def test_update_course_moderator(self):
-#         """Проверяет, что модератор может редактировать курс"""
-#         url = reverse("lms:courses-detail", args=[self.course.id])
-#         data = {"title": "Hacked Course"}
-#         response = self.client_mod.patch(url, data)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#
-#     def test_update_course_non_owner_and_non_moderator(self):
-#         """Проверяет, что пользователь не автор и не модератор не может редактировать курс"""
-#         url = reverse("lms:courses-detail", args=[self.course.id])
-#         data = {"title": "Hacked by stranger"}
-#         response = self.client_stranger.patch(url, data)
-#         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-#
-#     def test_delete_course_owner(self):
-#         """Проверяет, что автор курса может удалить курс"""
-#         url = reverse("lms:courses-detail", args=[self.course.id])
-#         response = self.client_user.delete(url)
-#         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-#         self.assertFalse(Course.objects.filter(id=self.course.id).exists())
-#
-#     def test_delete_course_normal_user(self):
-#         """Проверяет, что авторизованный пользователь не автор курса не может удалить курс"""
-#         url = reverse("lms:courses-detail", args=[self.course.id])
-#         response = self.client_stranger.delete(url)
-#         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-#
-#     def test_delete_course_moderator(self):
-#         """Проверяет, что модератор не может удалить курс"""
-#         url = reverse("lms:courses-detail", args=[self.course.id])
-#         response = self.client_mod.delete(url)
-#         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+class TestCourseViewSet(TestBaseLMSViewSet):
+    """Тестирует корректность работы CRUD курсов"""
+
+    def setUp(self):
+        """Формирует тестовые данные"""
+        super().setUp()
+        self.course = Course.objects.create(title="Test Course", owner=self.user)
+
+    def test_list_courses_authenticated(self):
+        """Проверяет, что отображается список курсов для авторизованных пользователей"""
+        url = reverse("lms:courses-list")
+        response = self.client_user.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_list_courses_unauthenticated(self):
+        """Проверяет, что не отображается список курсов для неавторизованных пользователей"""
+        url = reverse("lms:courses-list")
+        client = APIClient()
+        response = client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_retrieve_course_unauthenticated(self):
+        """Проверяет, что не отображается курс для неавторизованных пользователей"""
+        url = reverse("lms:courses-detail", args=[self.course.id])
+        client = APIClient()
+        response = client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_retrieve_course_non_owner_and_non_moderator(self):
+        """Проверяет, что авторизованный не автор и не модератор не может просматривать курс"""
+        url = reverse("lms:courses-detail", args=[self.course.id])
+        response = self.client_stranger.get(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_retrieve_course_owner(self):
+        """Проверяет, что отображается курс для автора"""
+        url = reverse("lms:courses-detail", args=[self.course.id])
+        response = self.client_user.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_course_moderator(self):
+        """Проверяет, что отображается курс для модератора"""
+        url = reverse("lms:courses-detail", args=[self.course.id])
+        response = self.client_mod.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_course_superuser(self):
+        """Проверяет, что админ может создавать курс"""
+        url = reverse("lms:courses-list")
+        data = {"title": "New Course"}
+        response = self.client_super.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Course.objects.filter(title="New Course").exists())
+
+    def test_create_course_normal_user(self):
+        """Проверяет, что авторизованный пользователь может создавать курс"""
+        url = reverse("lms:courses-list")
+        data = {"title": "User Course"}
+        response = self.client_user.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Course.objects.filter(title="User Course").exists())
+
+    def test_create_course_moderator(self):
+        """Проверяет, что модератор не может создавать курс"""
+        url = reverse("lms:courses-list")
+        data = {"title": "New Course"}
+        response = self.client_mod.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_update_course_owner(self):
+        """Проверяет, что автор курса может редактировать курс"""
+        url = reverse("lms:courses-detail", args=[self.course.id])
+        data = {"title": "Updated Course"}
+        response = self.client_user.patch(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.course.refresh_from_db()
+        self.assertEqual(self.course.title, "Updated Course")
+
+    def test_update_course_moderator(self):
+        """Проверяет, что модератор может редактировать курс"""
+        url = reverse("lms:courses-detail", args=[self.course.id])
+        data = {"title": "Hacked Course"}
+        response = self.client_mod.patch(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_course_non_owner_and_non_moderator(self):
+        """Проверяет, что пользователь не автор и не модератор не может редактировать курс"""
+        url = reverse("lms:courses-detail", args=[self.course.id])
+        data = {"title": "Hacked by stranger"}
+        response = self.client_stranger.patch(url, data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_course_owner(self):
+        """Проверяет, что автор курса может удалить курс"""
+        url = reverse("lms:courses-detail", args=[self.course.id])
+        response = self.client_user.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Course.objects.filter(id=self.course.id).exists())
+
+    def test_delete_course_normal_user(self):
+        """Проверяет, что авторизованный пользователь не автор курса не может удалить курс"""
+        url = reverse("lms:courses-detail", args=[self.course.id])
+        response = self.client_stranger.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_course_moderator(self):
+        """Проверяет, что модератор не может удалить курс"""
+        url = reverse("lms:courses-detail", args=[self.course.id])
+        response = self.client_mod.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
 class TestLessonViewSet(TestBaseLMSViewSet):
