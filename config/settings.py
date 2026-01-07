ecdb73d5333b78d336e32e024503b2682e058ae1
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2026-01-07 23:57:36 +0700

    Add default value for ALLOWED_HOSTS env

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 INSTALLED_APPS = [
