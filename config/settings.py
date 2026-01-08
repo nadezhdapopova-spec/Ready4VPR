a21f57be6661f6124d1318272a9a457bb41548f3
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2026-01-08 23:12:42 +0700

    Fix env vars naming

--- config/settings.py
+++ config/settings.py
@@ -9,11 +9,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(BASE_DIR / ".env", override=True)
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 
 INSTALLED_APPS = [
